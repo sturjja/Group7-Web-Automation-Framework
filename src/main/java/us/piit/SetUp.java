f53ae139af31c404e635ef3181355a5055b73527
sturjja <sturjja@gmail.com>
2023-05-08 23:47:14 -0400

    updated setup

--- src/main/java/us/piit/SetUp.java
+++ src/main/java/us/piit/SetUp.java
@@ -18,15 +18,57 @@ import java.time.Duration;
 public class SetUp {
     Logger log = LogManager.getLogger(SetUp.class.getName());
     WebDriver driver;
+    public void getCloudDriver(String envName, String os, String osVersion, String browserName, String browserVersion, String username, String password) throws MalformedURLException {
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability("os", os);
+        cap.setCapability("os_version", osVersion);
+        cap.setCapability("browser", browserName);
+        cap.setCapability("browser_version", browserVersion);
+        if (envName.equalsIgnoreCase("browserstack")){
+            cap.setCapability("resolution", "1024x768");
+            driver = new RemoteWebDriver(new URL("http://"+username+":"+password+"@hub-cloud.browserstack.com:80/wd/hub"),cap);
+        } else if (envName.equalsIgnoreCase("scaledupit")) {
+            driver = new RemoteWebDriver(new URL("http://"+username+":"+password+"@ondemand.automation.scaledupit.com:80/wd.hub"),cap);
+        }else if (envName.equalsIgnoreCase("freecrm")) {
+            driver = new RemoteWebDriver(new URL("http://"+username+":"+password+"@ondemand.freecrm.com:80/wd.hub"),cap);
+        }else if (envName.equalsIgnoreCase("mailchimp")) {
+            driver = new RemoteWebDriver(new URL("http://"+username+":"+password+"@ondemand.mailchimp.com:80/wd.hub"),cap);
+        }
+    }
 
+    public void getLocalDriver(String browserName){
+        if (browserName.equalsIgnoreCase("chrome")){
+            driver = new ChromeDriver();
+            log.info("chrome browser open success");
+        }else if (browserName.equalsIgnoreCase("firefox")){
+            driver = new FirefoxDriver();
+            log.info("firefox browser open success");
+        }else if (browserName.equalsIgnoreCase("edge")){
+            driver = new EdgeDriver();
+            log.info("edge browser open success");
+        }
+    }
 
-
-
-
-
-
-
-
+    @Parameters({"useCloudEnv","envName","os","osVersion","browserName","browserVersion","url"})
+    @BeforeMethod
+    public void setUp(@Optional("false") String useCloudEnv, @Optional("browserstack") String envName, @Optional("windows") String os,
+                      @Optional("10") String osVersion, @Optional("chrome") String browserName, @Optional("110") String browserVersion,
+                      @Optional("https://www.google.com") String url) throws MalformedURLException {
+        if (useCloudEnv.equalsIgnoreCase("true")){
+            getCloudDriver(envName,os,osVersion,browserName,browserVersion,"samiurturjja_uXPSKT","DcRHG1dHNfvnD4deAqPT");
+        } else if(useCloudEnv.equalsIgnoreCase("false")){
+            getLocalDriver(browserName);
+        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get(url);
+    }
+    @AfterMethod
+    public void tearDown(){
+        //close browser
+        driver.quit();
+        log.info("browser close success");
+    }
 
     //------------------------------------------------------------------------------------------------------------------
     //                                              selenium methods
